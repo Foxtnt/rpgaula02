e02a1f9d9d6a65d95c654fc6a90e007e2cc6f5df
BrunoCorrea <productbdc@gmail.com>
2025-08-14 20:43:45 -0300

    Adiciona Personagem

---
+++ Personagem.ts
@@ -0,0 +1,13 @@
+export class Personagem{
+    nome: string = "";
+    classe: string = "";
+    raca: string = "";
+    nivel: number = 0git add;
+    arma: string = "";
+    manaAtual: number = 0;
+    manaMaxima: number = 0;
+    vidaAtual: number = 0;
+    vidaMaxima: number = 0;
+    poderAtaque: number = 0;
+
+}
